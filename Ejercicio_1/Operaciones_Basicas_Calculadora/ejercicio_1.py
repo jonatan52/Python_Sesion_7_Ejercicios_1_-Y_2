fdcbe2c46e532664f4536355e3ecf985774698b5
jonatan52 <tarriba52@gmail.com>
2022-05-09 17:26:30 -0500

    Ejercicios 1 y 2 Sesion 7

---
+++ Ejercicio_1/Operaciones_Basicas_Calculadora/ejercicio_1.py
@@ -0,0 +1,20 @@
+#Ejercicio 1
+# Enunciado del ejercicio:
+
+# En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
+
+# Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.
+
+from operaciones import *
+a = 10
+b = 5
+print(f"Sumar: {a} + {b}  = {suma (a,b)}")
+
+print(f"Restar: {a} - {b} = {restar (a,b)}")
+
+print(f"multiplicar: {a} * {b}  = {multiplicar (a,b)}")
+
+print(f"Dividir: {a} / {b}  = {dividir (a,b)}")
+
+ 
+
---
+++ Ejercicio_2/Ejercicio_2.py
@@ -0,0 +1,20 @@
+# Ejercicio 2
+
+# Enunciado del ejercicio:
+
+# En este segundo ejercicios tendréis que crear un script que nos diga si es la hora de ir a casa. Tendréis que hacer uso del modulo time. 
+# Necesitaréis la fecha del sistema y poder comprobar la hora.
+
+# En el caso de que sean más de las 7, se mostrará un mensaje y en caso contrario, haréis una operación para calcular el tiempo que queda de trabajo.
+
+
+
+from datetime import datetime
+
+hora = datetime.now().strftime("%H:%M")
+print(f"Hora: {hora}")
+
+if int(hora[:2]) >= 19:
+    print("Es hora de irse a casa")
+else:
+    print(f"Quedan {18 - int(hora[:2])} horas y {59 - int(hora[3:])} minutos para irnos a casa")
